ca2a708d24df6388369b6aa9398f959360094341
Marcel Maciejczyk <mmaciejczyk@student.ethz.ch>
2024-03-21 21:38:22 +0100

    added Rust builtin factory

--- src/ir/__init__.py
+++ src/ir/__init__.py
@@ -2,11 +2,13 @@ from src.ir.kotlin_types import KotlinBuiltinFactory
 from src.ir.groovy_types import GroovyBuiltinFactory
 from src.ir.java_types import JavaBuiltinFactory
 from src.ir.scala_types import ScalaBuiltinFactory
+from src.ir.rust_types import RustBuiltinFactory
 
 
 BUILTIN_FACTORIES = {
     "kotlin": KotlinBuiltinFactory(),
     "groovy": GroovyBuiltinFactory(),
     "java": JavaBuiltinFactory(),
-    "scala": ScalaBuiltinFactory()
+    "scala": ScalaBuiltinFactory(),
+    "rust": RustBuiltinFactory()
 }
