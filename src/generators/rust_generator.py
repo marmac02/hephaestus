e51b55160ff21af9bc39a29e661b90b90ea2225b
Marcel Maciejczyk <mmaciejczyk@student.ethz.ch>
2024-07-27 22:46:07 +0200

    Revert type param restriction

--- src/generators/rust_generator.py
+++ src/generators/rust_generator.py
@@ -56,7 +56,6 @@ class RustGenerator(Generator):
         self.flag_Fn = False
         self.flag_FnMut = False
         self.flag_FnOnce = False
-        self.exclude_type_vars_closure = False
 
         #This flag is used for Rust to handle function calls in inner functions inside trait declarations
         self._inside_trait_decl = False
@@ -2113,8 +2112,6 @@ class RustGenerator(Generator):
                     continue
                 if rt.is_FnOnce(var_type) and (var.move_prohibited or self.inside_moving_lambda):
                     continue
-                if var.get_type().is_type_var() and self.exclude_type_vars_closure:
-                    continue
                     
             if not getattr(var_type, 'is_function_type', lambda: False)():
                 continue
@@ -2625,8 +2622,7 @@ class RustGenerator(Generator):
                                exclude_arrays=exclude_arrays,
                                exclude_covariants=exclude_covariants,
                                exclude_contravariants=exclude_contravariants,
-                               exclude_type_vars=exclude_type_vars or \
-                                (self.exclude_type_vars_closure and self.inside_moving_lambda),
+                               exclude_type_vars=exclude_type_vars,
                                exclude_function_types=exclude_function_types,
                                exclude_usr_types=exclude_usr_types)
         stype = ut.random.choice(types)
@@ -2907,9 +2903,7 @@ class RustGenerator(Generator):
             if ret_type == self.bt_factory.get_void_type()
             else ret_type
         )
-        self.exclude_type_vars_closure = (ret_type != self.bt_factory.get_void_type())
         expr = self.generate_expr(expr_type)
-        self.exclude_type_vars_closure = False
         self.move_semantics = prev_move_semantics
         decls = list(self.context.get_declarations(
             self.namespace, True).values())
